5c91ac593c4083e70c746701f92850f74ae9d6ba
7457 Prog Laptop 3 <frc7457@purdue.pphs.edu>
2024-02-03 14:38:29 -0500

    Added climber, elevator, limelight, shooter, and shooter angle subsysytems

---
+++ src/main/java/frc/robot/subsystems/ShooterAngle.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CANIDs;
+
+public class ShooterAngle extends SubsystemBase {
+  /** Creates a new ShooterAnfle. */
+  CANSparkMax tiltMotor = new CANSparkMax(CANIDs.ktiltMotor, MotorType.kBrushless);
+  public ShooterAngle() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
