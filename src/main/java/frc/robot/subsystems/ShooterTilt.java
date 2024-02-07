7bee4a213c5773d4d9658ddecf9ae78a98e82f3a
Rahul Yarlagadda <knufire@gmail.com>
2024-02-06 19:24:06 -0500

    Added tunable pid gains to shooter tilt

--- src/main/java/frc/robot/subsystems/ShooterTilt.java
+++ src/main/java/frc/robot/subsystems/ShooterTilt.java
@@ -4,6 +4,11 @@
 
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.littletonrobotics.util.LoggedTunableNumber;
+
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -24,6 +29,26 @@ public class ShooterTilt extends SubsystemBase {
   DutyCycleEncoder tiltAbsoluteEncoder;
   TalonFXConfiguration tiltConfig;
 
+  private static final LoggedTunableNumber kp = new LoggedTunableNumber("ShooterTilt/Kp");
+  private static final LoggedTunableNumber ki = new LoggedTunableNumber("ShooterTilt/Ki");
+  private static final LoggedTunableNumber kd = new LoggedTunableNumber("ShooterTilt/Kd");
+  private static final LoggedTunableNumber ks = new LoggedTunableNumber("ShooterTilt/Ks");
+  private static final LoggedTunableNumber kv = new LoggedTunableNumber("ShooterTilt/Kv");
+  private static final LoggedTunableNumber ka = new LoggedTunableNumber("ShooterTilt/Ka");
+  private static final LoggedTunableNumber kg = new LoggedTunableNumber("ShooterTilt/Kg");
+  private static final List<LoggedTunableNumber> pidGains = new ArrayList<>();
+
+  static {
+    kp.initDefault(0.0);
+    ki.initDefault(0.0);
+    kd.initDefault(0.0);
+    ks.initDefault(0.0);
+    kv.initDefault(0.0);
+    ka.initDefault(0.0);
+    kg.initDefault(0.0);
+    pidGains.addAll(List.of(kp, ki, kd, ks, kv, ka, kg));
+  }
+
   public ShooterTilt() {
     tiltMotor = new TalonFX(CANIDs.kTiltMotor);
     configureTalonFx(tiltMotor);
@@ -36,6 +61,23 @@ public class ShooterTilt extends SubsystemBase {
 
   @Override
   public void periodic() {
+    for (LoggedTunableNumber gain : pidGains) {
+      if (gain.hasChanged(hashCode())) {
+        // Send new PID gains to talon
+        Slot0Configs slot0config =
+        new Slot0Configs()
+            .withGravityType(GravityTypeValue.Arm_Cosine)
+            .withKP(kp.get())
+            .withKI(ki.get())
+            .withKD(kd.get())
+            .withKS(ks.get())
+            .withKV(kv.get())
+            .withKA(ka.get())
+            .withKG(kg.get());
+        tiltMotor.getConfigurator().apply(slot0config);
+        break;
+      }
+    }
     // This method will be called once per scheduler run
   }
 
@@ -49,13 +91,13 @@ public class ShooterTilt extends SubsystemBase {
     Slot0Configs slot0config =
         new Slot0Configs()
             .withGravityType(GravityTypeValue.Arm_Cosine)
-            .withKP(0)
-            .withKI(0)
-            .withKD(0)
-            .withKS(0)
-            .withKV(0)
-            .withKA(0)
-            .withKG(0);
+            .withKP(kp.get())
+            .withKI(ki.get())
+            .withKD(kd.get())
+            .withKS(ks.get())
+            .withKV(kv.get())
+            .withKA(ka.get())
+            .withKG(kg.get());
     SoftwareLimitSwitchConfigs softlimitConfig =
         new SoftwareLimitSwitchConfigs()
             .withForwardSoftLimitThreshold(TiltConstants.kForwardSoftLimit)
