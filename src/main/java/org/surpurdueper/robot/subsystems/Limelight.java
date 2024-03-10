3300d3de2d568cd7e0916d7437f5f81eb1e96aaf
7457StudentLaptop2 <supurdueperrobotics+1@pphs.purdue.edu>
2024-03-10 13:41:30 -0400

    limelight stuff

--- src/main/java/org/surpurdueper/robot/subsystems/Limelight.java
+++ src/main/java/org/surpurdueper/robot/subsystems/Limelight.java
@@ -4,14 +4,35 @@
 
 package org.surpurdueper.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.littletonrobotics.util.VirtualSubsystem;
+import org.surpurdueper.robot.utils.LimelightHelpers;
 
-public class Limelight extends VirtualSubsystem {
-  /** Creates a new Limelight. */
-  public Limelight() {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimeLight extends VirtualSubsystem {
+  /** Creates a new LimeLight. */
+  public LimeLight() {
+    double tx = LimelightHelpers.getTX("limelight");
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+NetworkTableEntry tx = table.getEntry("tx");
+NetworkTableEntry ty = table.getEntry("ty");
+NetworkTableEntry ta = table.getEntry("ta");
+
+//read values periodically
+double x = tx.getDouble(0.0);
+double y = ty.getDouble(0.0);
+double area = ta.getDouble(0.0);
+
+//post to smart dashboard periodically
+SmartDashboard.putNumber("LimelightX", x);
+SmartDashboard.putNumber("LimelightY", y);
   }
 }
