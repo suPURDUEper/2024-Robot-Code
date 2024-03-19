677c004861e6081c211f637de3a557c392bc753c
7457StudentLaptop2 <supurdueperrobotics+1@pphs.purdue.edu>
2024-03-19 16:44:59 -0400

    Add new autos

---
+++ src/main/java/org/surpurdueper/robot/commands/auto/ThreeDiskSource.java
@@ -0,0 +1,32 @@
+package org.surpurdueper.robot.commands.auto;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import org.surpurdueper.robot.subsystems.Elevator;
+import org.surpurdueper.robot.subsystems.Intake;
+import org.surpurdueper.robot.subsystems.Limelight;
+import org.surpurdueper.robot.subsystems.Shooter;
+import org.surpurdueper.robot.subsystems.ShooterTilt;
+import org.surpurdueper.robot.subsystems.drive.CommandSwerveDrivetrain;
+
+public class ThreeDiskSource extends SequentialCommandGroup {
+
+  public ThreeDiskSource(
+      CommandSwerveDrivetrain drivetrain,
+      Intake intake,
+      ShooterTilt shooterTilt,
+      Shooter shooter,
+      Elevator elevator,
+      Limelight limelight) {
+
+    PathPlannerPath toThirdDisk = PathPlannerPath.fromChoreoTrajectory("5 source side.3");
+
+    addCommands(
+        new TwoDiskSource(drivetrain, intake, shooterTilt, shooter, elevator, limelight),
+        Commands.deadline(AutoBuilder.followPath(toThirdDisk), intake.load()),
+        Autos.aimAndFireNoElevator(drivetrain, shooterTilt, elevator, shooter, limelight, intake)
+            .withTimeout(0.75));
+  }
+}
---
+++ src/main/java/org/surpurdueper/robot/commands/auto/TwoDiskSource.java
@@ -0,0 +1,48 @@
+package org.surpurdueper.robot.commands.auto;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import org.littletonrobotics.util.AllianceFlipUtil;
+import org.surpurdueper.robot.Constants.TiltConstants;
+import org.surpurdueper.robot.subsystems.Elevator;
+import org.surpurdueper.robot.subsystems.Intake;
+import org.surpurdueper.robot.subsystems.Limelight;
+import org.surpurdueper.robot.subsystems.Shooter;
+import org.surpurdueper.robot.subsystems.ShooterTilt;
+import org.surpurdueper.robot.subsystems.drive.CommandSwerveDrivetrain;
+
+public class TwoDiskSource extends SequentialCommandGroup {
+  /** Creates a new FourDiskSource. */
+  public TwoDiskSource(
+    CommandSwerveDrivetrain drivetrain,
+    Intake intake,
+    ShooterTilt shooterTilt,
+    Shooter shooter,
+    Elevator elevator,
+    Limelight limelight) {
+      PathPlannerPath lineupFirstShot = PathPlannerPath.fromChoreoTrajectory("5 source side.1");
+      PathPlannerPath linupSecondShot = PathPlannerPath.fromChoreoTrajectory("5 source side.2");
+
+       Rotation2d startingHeading = Rotation2d.fromRadians(1.5551732981225221);
+    Pose2d startingPose =
+        new Pose2d(
+            lineupFirstShot.getPreviewStartingHolonomicPose().getTranslation(), startingHeading);
+
+     addCommands(
+        Commands.runOnce(() -> drivetrain.seedFieldRelative(AllianceFlipUtil.apply(startingPose))),
+        shooter.on(),
+        AutoBuilder.followPath(lineupFirstShot),
+        Autos.aimAndFireWithElevator(drivetrain, shooterTilt, elevator, shooter, limelight, intake)
+            .withTimeout(0.75),
+        shooterTilt.goToPositionBlocking(TiltConstants.kIntakeAngle),
+        Commands.deadline(AutoBuilder.followPath(linupSecondShot), intake.load()),
+        Autos.aimAndFireWithElevator(drivetrain, shooterTilt, elevator, shooter, limelight, intake)
+            .withTimeout(0.75));
+  }
+}
