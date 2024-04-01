4fe0657c1c240d8116b3a5551f9f7b56007dbd53
Rahul Yarlagadda <knufire@gmail.com>
2024-04-01 19:42:05 -0400

    More cleanup

--- src/main/java/org/surpurdueper/robot/subsystems/Limelight.java
+++ src/main/java/org/surpurdueper/robot/subsystems/Limelight.java
@@ -5,13 +5,17 @@
 package org.surpurdueper.robot.subsystems;
 
 import edu.wpi.first.math.MathSharedStore;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.TimeInterpolatableBuffer;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.Optional;
+
+import org.littletonrobotics.util.FieldConstants;
 import org.littletonrobotics.util.VirtualSubsystem;
 import org.surpurdueper.robot.Constants.LookupTables;
 import org.surpurdueper.robot.subsystems.drive.CommandSwerveDrivetrain;
@@ -78,48 +82,48 @@ public class Limelight extends VirtualSubsystem {
     return Optional.of(angleToGoal);
   }
 
-  // private void updatePose2DAprilTag() {
-  //   // To do any processing we need to know what alliance we are on and if we have a valid target
-  //   if (DriverStation.getAlliance().isEmpty() || !LimelightHelpers.getTV("")) {
-  //     return;
-  //   }
-  //   double ty = LimelightHelpers.getTY("");
-  //   double tx = LimelightHelpers.getTX("");
-  //   double latencyMs =
-  //       LimelightHelpers.getLatency_Capture("") + LimelightHelpers.getLatency_Pipeline("");
+  private void updatePose2DAprilTag() {
+    // To do any processing we need to know what alliance we are on and if we have a valid target
+    if (DriverStation.getAlliance().isEmpty() || !LimelightHelpers.getTV("")) {
+      return;
+    }
+    double ty = LimelightHelpers.getTY("");
+    double tx = LimelightHelpers.getTX("");
+    double latencyMs =
+        LimelightHelpers.getLatency_Capture("") + LimelightHelpers.getLatency_Pipeline("");
 
-  //   if (latencyMs / 1000.0 > kBufferDuration) {
-  //     return;
-  //   }
+    if (latencyMs / 1000.0 > kBufferDuration) {
+      return;
+    }
 
-  //   double timestamp = MathSharedStore.getTimestamp() - latencyMs / 1000.0;
-  //   // Calculate how far we are from the apriltag
-  //   double distance = getDistanceToGoalMeters().get();
+    double timestamp = MathSharedStore.getTimestamp() - latencyMs / 1000.0;
+    // Calculate how far we are from the apriltag
+    double distance = getDistanceToGoalMeters().get();
 
-  //   // Calculate the angle from the apriltag to the camera. Account for the delay in the camera
-  //   // capture and processing
-  //   Optional<Rotation2d> robotAngleOptional = robotAngleBuffer.getSample(timestamp);
-  //   Rotation2d robotAngle =
-  //       robotAngleOptional.orElseGet(() -> drivetrain.getState().Pose.getRotation());
-  //   Rotation2d angle = robotAngle.minus(Rotation2d.fromDegrees(tx));
-  //   SmartDashboard.putNumber("Vision/AngleToGoal (deg)", angle.getDegrees());
+    // Calculate the angle from the apriltag to the camera. Account for the delay in the camera
+    // capture and processing
+    Optional<Rotation2d> robotAngleOptional = robotAngleBuffer.getSample(timestamp);
+    Rotation2d robotAngle =
+        robotAngleOptional.orElseGet(() -> drivetrain.getState().Pose.getRotation());
+    Rotation2d angle = robotAngle.minus(Rotation2d.fromDegrees(tx));
+    SmartDashboard.putNumber("Vision/AngleToGoal (deg)", angle.getDegrees());
 
-  //   // Make a vector using the previous distance and angle and calculate the camera position
-  //   Translation2d apriltag =
-  //       (DriverStation.getAlliance().get() == Alliance.Red)
-  //           ? FieldConstants.aprilTag4
-  //           : FieldConstants.aprilTag7;
-  //   Translation2d tagToCameraTranslation = new Translation2d(distance, angle);
-  //   Translation2d cameraToRobotCenterTranslation =
-  //       new Translation2d(0, robotAngle);
-  //   Translation2d robot =
-  //       apriltag.plus(tagToCameraTranslation).plus(cameraToRobotCenterTranslation);
-  //   Pose2d poseEstimate = new Pose2d(robot, robotAngle);
-  //   SmartDashboard.putNumberArray(
-  //       "Vision/Pose Estimate",
-  //       new double[] {
-  //         poseEstimate.getX(), poseEstimate.getY(), poseEstimate.getRotation().getRotations()
-  //       });
-  //   // drivetrain.addVisionMeasurement(poseEstimate, timestamp);
-  // }
+    // Make a vector using the previous distance and angle and calculate the camera position
+    Translation2d apriltag =
+        (DriverStation.getAlliance().get() == Alliance.Red)
+            ? FieldConstants.aprilTag4
+            : FieldConstants.aprilTag7;
+    Translation2d tagToCameraTranslation = new Translation2d(distance, angle);
+    Translation2d cameraToRobotCenterTranslation =
+        new Translation2d(0, robotAngle);
+    Translation2d robot =
+        apriltag.plus(tagToCameraTranslation).plus(cameraToRobotCenterTranslation);
+    Pose2d poseEstimate = new Pose2d(robot, robotAngle);
+    SmartDashboard.putNumberArray(
+        "Vision/Pose Estimate",
+        new double[] {
+          poseEstimate.getX(), poseEstimate.getY(), poseEstimate.getRotation().getRotations()
+        });
+    drivetrain.addVisionMeasurement(poseEstimate, timestamp);
+  }
 }
